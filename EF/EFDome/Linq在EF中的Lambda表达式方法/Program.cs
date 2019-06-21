b398b1f100c92fff699f8c2ea694bfc887537ded
17875 <17875@>
2019-06-21 16:21:13 +0800

    Linq在EF中的Lambda表达式方法

---
+++ EF/EFDome/Linq在EF中的Lambda表达式方法/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq在EF中的Lambda表达式方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void sele() {
+
+            using (SchoolDBEntities context = new SchoolDBEntities())
+            {
+                context.StudentSets.Select(i => i);
+            }
+
+        }
+    }
+}
