f8030c9aa459b4f679ab8ff550e281295d80a703
17875 <17875@>
2019-06-25 18:46:25 +0800

    EDM可视化管理的一些操作

---
+++ EF/EFDome/EDM提供的一些功能/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDM提供的一些功能
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void EDMTran() {
+
+            using (EFDomeEntities context = new EFDomeEntities())
+            {
+                //context.存储过程方法
+            }
+        }
+
+    }
+}
