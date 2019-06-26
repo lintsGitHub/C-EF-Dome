581ed775e2959e69bd852ca782ac536b0f144a24
17875 <17875@>
2019-06-26 11:41:41 +0800

    EF中的拦截

---
+++ EF/EFDome/EF中的拦截/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF中的拦截
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /// <summary>
+    /// 数据库的CURD都经过这里，这个就是一个拦截器
+    /// </summary>
+    public class EFCommandIntercoptor : IDbCommandInterceptor
+    {
+        public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
